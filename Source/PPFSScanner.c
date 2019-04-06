1f21f66e7b5223942a7f77cad47e614989c08884
woodensalmon <45913039+woodensalmon@users.noreply.github.com>
2019-04-06 21:23:51 +0200

    kilka funkcji
    jestem w trakcie debugowania funkcji skanujacej pliki do usuniecia. trzeba do niej dodac samo usuwanie pliku

--- Source/PPFSScanner.c
+++ Source/PPFSScanner.c
@@ -3,3 +3,33 @@
 #include "PPFSScanner.h"
 
 #include "PPFSPrerequisites.h"
+
+char* setFilePath(char *fileName,char* folderPath)
+{
+
+	int lenght =strlen(fileName) + strlen(folderPath)+1;
+
+	char *filePath = (char*) malloc(lenght);
+
+	strcpy(filePath,folderPath);
+    strcat(filePath,"/");
+	strcat(filePath,fileName);
+
+	return filePath;
+}
+struct stat getStatFile(char* filePath)
+{
+	char buf[200];
+	struct stat fileinfo;
+	int data = stat(filePath,&fileinfo);
+	return fileinfo;
+}
+
+int doesFileExist(const char* filename){
+    struct stat buffer;
+    int exist = stat(filename,&buffer);
+    if(exist == 0)
+        return 1;
+    else // -1
+        return 0;
+}
