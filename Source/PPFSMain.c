4d5c712081cb104da9b65bce2a007014615eb91c
woodensalmon <45913039+woodensalmon@users.noreply.github.com>
2019-04-06 21:53:27 +0200

    petla glowna i parser argumentow main

--- Source/PPFSMain.c
+++ Source/PPFSMain.c
@@ -7,6 +7,53 @@ void handler(int signum)
 
 int main(void)
 {
-	printf("IT WORKS!!!");
-	return 0;
+	char *source=NULL;
+    char *destination=NULL;
+	int sleepTime = 5 * 60;
+	int recurency=0;
+	int threshold = 100;
+	int argument=0;
+	while((argument=getopt(argc,argv,"s:d:w:t:R"))!=-1)
+	{
+		//source destination sleep(wait) recurency threshold
+		switch(argument)
+		{
+        	case 's':
+			source = optarg;
+//printf("source: %s\n",optarg);
+
+		break;
+
+		case 'd':
+			destination=optarg;
+//printf("destination: %s\n",optarg);
+		break;
+
+		case 'w':
+			sleepTime = atoi(optarg);
+//printf("sleep time: %s\n",optarg);
+		break;
+
+		case 'R':
+			recurency=1;
+			printf("recurency\n");
+		break;
+
+		case 't':
+			threshold = atoi(optarg);
+//printf("threshold: %s\n",optarg);
+		break;
+
+		}
+
+	}
+
+		//return 0;
+	printf("     -----BEGIN OF PROGRAM-----\n");
+	while(1)
+	{
+        deleteAdditionalItems(source, destination,recurency);
+
+	}
+
 }
