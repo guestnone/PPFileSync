935c39157c8a31f564a89ab169ffcdca310417d3
woodensalmon <45913039+woodensalmon@users.noreply.github.com>
2019-04-19 15:29:49 +0200

    added logging

--- Source/PPFSScanner.c
+++ Source/PPFSScanner.c
@@ -64,7 +64,22 @@ void deleteAdditionalItems(char* source_path, char* destination_path,int recuren
                         {
 
                             deleteAdditionalItems(setElementPath(file->d_name,source_path),setElementPath(file->d_name,destination_path),recurency);
-                             rmdir(setElementPath(file->d_name,destination_path));
+
+                            int rmdirStatus =0;
+                            rmdirStatus = rmdir(setElementPath(file->d_name,destination_path));
+                             if(rmdirStatus==0)
+                             {
+                                time_t t = time(NULL);
+                                struct tm tm = *localtime(&t);
+                                LOGINFO("%d-%d-%d %d:%d:%d:directory doesnt exist in source folder. deleted directory succesfull: %s\n", tm.tm_year + 1900, tm.tm_mon + 1, tm.tm_mday, tm.tm_hour, tm.tm_min, tm.tm_sec,file->d_name);
+                                //LOGINFO("abcdefghijk %d",11);
+                             }
+                             else
+                             {
+                                time_t t = time(NULL);
+                                struct tm tm = *localtime(&t);
+                                LOGINFO("%d-%d-%d %d:%d:%d: directory doesnt exist in source folder. couldnt delete directory: %s\n", tm.tm_year + 1900, tm.tm_mon + 1, tm.tm_mday, tm.tm_hour, tm.tm_min, tm.tm_sec,file->d_name);
+                             }
 
                         }
 
@@ -78,10 +93,22 @@ void deleteAdditionalItems(char* source_path, char* destination_path,int recuren
         }
         else
         {
-            
+
             if(!element_exist (setElementPath(file->d_name,source_path)) )
             {
                 //delete file
+                if(0/*deleted*/)
+                {
+                    time_t t = time(NULL);
+                    struct tm tm = *localtime(&t);
+                    LOGINFO("%d-%d-%d %d:%d:%d:file doesnt exist in source folder. succesfully deleted file: %s\n", tm.tm_year + 1900, tm.tm_mon + 1, tm.tm_mday, tm.tm_hour, tm.tm_min, tm.tm_sec,file->d_name);
+                }
+                else
+                {
+                    time_t t = time(NULL);
+                    struct tm tm = *localtime(&t);
+                    LOGINFO("%d-%d-%d %d:%d:%d:file doesnt exist in source folder. couldnt delete file: %s \n", tm.tm_year + 1900, tm.tm_mon + 1, tm.tm_mday, tm.tm_hour, tm.tm_min, tm.tm_sec,file->d_name);
+                }
                 printf("file does not exist: %s\n",file->d_name);
             }
             else
@@ -90,6 +117,19 @@ void deleteAdditionalItems(char* source_path, char* destination_path,int recuren
                 if(shouldIdeleteElement(getStatFile(setElementPath(file->d_name,source_path)),getStatFile(setElementPath(file->d_name,destination_path))))
                 {
                     //delete file
+                    if(0/*deleted*/)
+                    {
+                        time_t t = time(NULL);
+                        struct tm tm = *localtime(&t);
+                         LOGINFO("%d-%d-%d %d:%d:%d: file is old. succesfully deleted file: %s  \n", tm.tm_year + 1900, tm.tm_mon + 1, tm.tm_mday, tm.tm_hour, tm.tm_min, tm.tm_sec,file->d_name);
+                    }
+                   else
+                   {
+                    time_t t = time(NULL);
+                    struct tm tm = *localtime(&t);
+                    LOGINFO("%d-%d-%d %d:%d:%d: file is old. couldnt delete file: %s  \n", tm.tm_year + 1900, tm.tm_mon + 1, tm.tm_mday, tm.tm_hour, tm.tm_min, tm.tm_sec,file->d_name);
+
+                   }
                     printf("file is old: %s\n",file->d_name);
                 }
                 else printf("file is good: %s\n",file->d_name);
@@ -119,7 +159,22 @@ void copyPasteElements(char* source_path, char* destination_path,int recurency,i
                             printf("creating dir: %s\n",file->d_name);
                             struct stat toGetModeT;
                             fstat(setElementPath(file->d_name,source_path),&toGetModeT);
-                            mkdir(setElementPath(file->d_name,destination_path),toGetModeT.st_mode);
+                            int mkdirStatus = mkdir(setElementPath(file->d_name,destination_path),toGetModeT.st_mode);
+
+                            if(mkdirStatus==0)
+                            {
+                                time_t t = time(NULL);
+                                struct tm tm = *localtime(&t);
+                                LOGINFO("%d-%d-%d %d:%d:%d: created directory: %s  \n", tm.tm_year + 1900, tm.tm_mon + 1, tm.tm_mday, tm.tm_hour, tm.tm_min, tm.tm_sec,file->d_name);
+
+                            }
+                            else
+                            {
+                                time_t t = time(NULL);
+                                struct tm tm = *localtime(&t);
+                                LOGINFO("%d-%d-%d %d:%d:%d: couldnt create directory: %s  \n", tm.tm_year + 1900, tm.tm_mon + 1, tm.tm_mday, tm.tm_hour, tm.tm_min, tm.tm_sec,file->d_name);
+
+                            }
                             //create dir
                             copyPasteElements(setElementPath(file->d_name,source_path),setElementPath(file->d_name,destination_path),recurency,threshold);
                         }
@@ -142,6 +197,21 @@ void copyPasteElements(char* source_path, char* destination_path,int recurency,i
                 if(threshold < buf.st_size)
                 {
                     //copypasteBigFile()
+
+                    if(0/*sucessful*/)
+                    {
+                        time_t t = time(NULL);
+                        struct tm tm = *localtime(&t);
+                        LOGINFO("%d-%d-%d %d:%d:%d: copied and pasted file: %s. size of file: %ld  \n", tm.tm_year + 1900, tm.tm_mon + 1, tm.tm_mday, tm.tm_hour, tm.tm_min, tm.tm_sec,file->d_name,buf.st_size);
+
+                    }
+                    else
+                    {
+                        time_t t = time(NULL);
+                        struct tm tm = *localtime(&t);
+                        LOGINFO("%d-%d-%d %d:%d:%d: couldnt copy paste file: %s. size of file: %ld  \n", tm.tm_year + 1900, tm.tm_mon + 1, tm.tm_mday, tm.tm_hour, tm.tm_min, tm.tm_sec,file->d_name,buf.st_size);
+
+                    }
                 }
                 else
                 {
